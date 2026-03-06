d29a4a6eb815b44854eadf83ed1d922c52e3deb0
Temu-bit <timeosanta22@gmail.com>
2026-03-07 00:26:41 +0100

    Add files via upload
    Update

--- products.js
+++ products.js
@@ -16,26 +16,4 @@ const categories = [
     }
 ];
 
-const products = [
-    {
-        "id": 1772757331045,
-        "name": "Santa Blanca Jersey",
-        "price": "67,90€",
-        "category": "men",
-        "link": "https://www.spized.com/de/checkout/pre-cart"
-    },
-    {
-        "id": 1772757342395,
-        "name": "Santa Blanca Hoodie OG",
-        "price": "47,99€",
-        "category": "men",
-        "link": "https://www.spreadshirt.de/selbst-gestalten"
-    },
-    {
-        "id": 1772758273557,
-        "name": "Santa Blanca Jersey",
-        "price": "67,90€",
-        "category": "men",
-        "link": "https://www.spized.com/de/checkout/pre-cart"
-    }
-];
+const products = [];
