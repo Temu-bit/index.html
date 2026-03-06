ee05e84b1f65641ae900b6836b8de47d173e4e3e
Temu-bit <timeosanta22@gmail.com>
2026-03-06 01:44:40 +0100

    Add files via upload
    Update Produkte

--- products.js
+++ products.js
@@ -1,7 +1,34 @@
 const categories = [
-    { id: 'men', name: 'Herren', icon: '👕' },
-    { id: 'women', name: 'Damen', icon: '👗' },
-    { id: 'accessories', name: 'Accessoires', icon: '🕶️' }
+    {
+        "id": "men",
+        "name": "Herren",
+        "icon": "👕"
+    },
+    {
+        "id": "women",
+        "name": "Damen",
+        "icon": "👗"
+    },
+    {
+        "id": "accessories",
+        "name": "Accessoires",
+        "icon": "🕶️"
+    }
 ];
 
-const products = [];
+const products = [
+    {
+        "id": 1772757331045,
+        "name": "Santa Blanca Jersey",
+        "price": "67,90€",
+        "category": "men",
+        "link": "https://www.spized.com/de/checkout/pre-cart"
+    },
+    {
+        "id": 1772757342395,
+        "name": "Santa Blanca Hoodie OG",
+        "price": "47,99€",
+        "category": "men",
+        "link": "https://www.spreadshirt.de/selbst-gestalten"
+    }
+];
